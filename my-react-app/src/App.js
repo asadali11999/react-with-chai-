afa8a1a000137949387ec8c715daebf0f5ee328e
asad ali <asad3837@gmail.com>
2023-12-17 14:29:08 +0500

    ppushing my git

---
+++ my-react-app/src/App.js
@@ -0,0 +1,42 @@
+import {useState} from 'react';
+
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    let [counter , setcounter] = useState(15)
+    const addValue = () => {
+        setcounter (counter + 1);
+        console.log('clicked' , counter);  
+        if (counter === 20 ){
+          setcounter (counter = 1 );
+        }
+
+    }
+    const removeValue = () =>{
+      setcounter (counter - 1)
+
+
+      if (counter === 0){
+        setcounter (counter = 1);
+      }
+    }
+
+
+  return (
+    <>
+    <h1>chai aur react</h1>
+    <h2> counter value : {counter}</h2>
+    <button onClick={addValue}>add value</button>
+    <button
+     onClick={removeValue}>remove value</button>
+
+
+    
+    
+    </>
+  );
+}
+
+export default App;
